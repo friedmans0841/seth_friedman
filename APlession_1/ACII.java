fa9c4854bb74d98cb205a1125a1fa0fc9efdfaa9
friedmans0841 <FriedmanS0841@sduhsd.lan>
2016-09-08 07:58:10 -0700

    added files

---
+++ APlession_1/ACII.java
@@ -0,0 +1,11 @@
+public class ACII
+{
+	public static void main (String[]args)
+
+		System.out.println("H   H EEE L     L      OOO       W   W  OOO  RRRR  L     DDDD  !!");
+		System.out.println("H   H E     L     L     O   O      W W W O   O R   R L     D   D !!");
+		System.out.println("HHH EEE L     L     O   O      W W W O   O RRRR  L     D   D !!");
+		System.out.println("H   H E     L     L     O   O  ,,   W W  O   O R   R L     D   D !!");
+		System.out.println("H   H EEE LLL LLL  OOO  ,,    W W   OOO  R   R LLL DDDD  !!");
+	}
+}
